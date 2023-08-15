276a37725c2805724fe187c38d1ef68ddafc2de0
ixs <69159791+300ixs@users.noreply.github.com>
2023-08-15 23:44:39 +0200

    Update install_docker_and_portainer.sh

--- install_docker_and_portainer.sh
+++ install_docker_and_portainer.sh
@@ -1,17 +1,28 @@
+#!/bin/bash
 
-DIST_BASE="debian"
+# Update the package index
+sudo apt update
 
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+# Install prerequisites
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.docker.com/linux/$DIST_BASE/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
+# Add Docker GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/$DIST_BASE $(lsb_release -cs) stable"
-apt-get install -y docker-ce docker-ce-cli containerd.io
+# Add Docker repository
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-docker run hello-world
+# Update the package index again
+sudo apt update
+
+# Install Docker
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Add the current user to the "docker" group to avoid using sudo with Docker commands
+sudo usermod -aG docker $USER
+
+# Install Portainer via Docker
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
-apt-get install -y docker-compose
+echo "Docker and Portainer have been installed. Please log out and log back in to use Docker without sudo."
