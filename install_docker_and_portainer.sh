7271e3f96712b7524759beec0160259d71c4e7b7
wicksei <69159791+wicksei@users.noreply.github.com>
2023-08-18 20:42:26 +0200

    Update install_docker_and_portainer.sh

--- install_docker_and_portainer.sh
+++ install_docker_and_portainer.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install -y curl
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
