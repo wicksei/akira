9ded8b81a1bc8645c526a78a7a046903a8704e97
wicksei <69159791+wicksei@users.noreply.github.com>
2023-09-21 18:32:51 +0200

    Update install_docker_and_portainer.sh

--- install_docker_and_portainer.sh
+++ install_docker_and_portainer.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+# Install docker and all necessary packages
 sudo apt update
 sudo apt install -y curl
 sudo apt install -y apt-transport-https ca-certificates
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install docker
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Run docker without the need for root
 sudo usermod -aG docker $USER
+
+# Install Portainer
 docker volume create portainer_data
 sudo docker pull portainer/portainer-ce
 docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
